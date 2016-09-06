aa5346db6b314e3ab009bb14c4d92bc107b7a4b9
Supanut Apikulvanich <Supanut.Overload@gmail.com>
2016-09-07 00:05:47 +0700

    Fixed Error 304

--- src/app.js
+++ src/app.js
@@ -50,6 +50,7 @@ app.use((req, res, next) => {
 });
 app.use(lusca.xframe('SAMEORIGIN'));
 app.use(lusca.xssProtection(true));
+app.disable('etag');
 app.use(cors());
 app.use(authenticator);
 app.use('/', routes);
