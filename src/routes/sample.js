f332fcc77ef97f483da4cd22b27ceb375ea992e5
Supanut Apikulvanich <supanut.overload@gmail.com>
2016-09-07 22:54:22 +0700

    Upload file with multer

--- src/routes/sample.js
+++ src/routes/sample.js
@@ -1,9 +1,34 @@
 import { Router } from 'express';
+import multer from 'multer';
 
 const router = Router();
+const LIMIT_10_MB = 1000 * 1000 * 10;
+
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    console.log(file);
+    cb(null, './uploads');
+  },
+  filename: (req, file, cb) => {
+    cb(null, file.fieldname + '-' + Date.now());
+  }
+});
+const upload = multer({
+  storage: storage,
+  limits: {
+    fileSize: LIMIT_10_MB,
+    files: 1
+    // fields: 2
+  }
+});
 router.get('/', (req, res) => {
   console.log('============');
   res.send('okkkk');
+});
+
+router.post('/upload', upload.single('file'), (req, res) => {
+  console.log(req.body);
+  res.send('Upload Success');
 });
 
 export default router;
